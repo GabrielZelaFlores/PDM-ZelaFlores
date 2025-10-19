8cdad737fe3c869098c633f75647ff04b9539014
GabrielZelaFlores <gzelaf@ulasalle.edu.pe>
2025-10-19 15:36:19 -0500

    Agregando actividad principal del juego MainActivity.kt

--- PARCIAL-PDM-ZelaFlores/app/MainActivity.kt
+++ PARCIAL-PDM-ZelaFlores/app/MainActivity.kt
@@ -1,7 +1,7 @@
 /*
 Autor: Gabriel Frank Krisna Zela Flores
-Descripción: Esta es la actividad principal del juego, donde se encuentra el NavHostFragment.
-              Básicamente se encarga de manejar la navegación entre las diferentes pantallas (fragments)
+Descripción: Esta es la actividad principal del juego, donde se encuentra el NavHostFragment, 
+básicamente se encarga de manejar la navegación entre las diferentes pantallas (fragments)
 Fecha creación: 16/10/2025
 Última modificación: 16/10/2025
 */

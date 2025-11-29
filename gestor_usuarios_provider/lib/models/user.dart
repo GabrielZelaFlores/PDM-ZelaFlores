70cf5ead3a690cab7952b059eb7e7e9699b6f52e
GabrielZelaFlores <gzelaf@ulasalle.edu.pe>
2025-11-28 22:10:01 -0500

    Add files via upload

---
+++ gestor_usuarios_provider/lib/models/user.dart
@@ -0,0 +1,15 @@
+// Autor: Gabriel Frank Krisna Zela Flores
+// Fecha de creación: 28/11/2025
+// Última actualización: 28/11/2025
+
+class User {
+  String nombre;
+  String genero; // 'Masculino' o 'Femenino'
+  bool activo;
+
+  User({
+    required this.nombre,
+    required this.genero,
+    required this.activo,
+  });
+}
